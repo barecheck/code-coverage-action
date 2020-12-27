50023e91d8b62960e4961cd48b19260e326a8c5b
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-12-27 20:09:42 +0000

    init javascript based github action

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true
+  },
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {}
+};
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # code-coverage-action
+
 GitHub Action that posts coverage as comment in Pull request
