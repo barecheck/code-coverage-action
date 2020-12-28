0f4cc45d1923c3eaa20c309241d99ff3ab7c9d0d
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-12-28 15:57:51 +0000

    add arrow

--- dist/index.js
+++ dist/index.js
@@ -5909,13 +5909,14 @@ const github = __webpack_require__(438);
 
 const sendComment = async (token, diff, totalCoverage) => {
   const octokit = github.getOctokit(token);
+  const arrow = diff === 0 ? '' : diff < 0 ? '▾' : '▴';
 
   if (github.context.payload.pull_request) {
     await octokit.issues.createComment({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       issue_number: github.context.payload.pull_request.number,
-      body: `<h3>Code coverage report</h3>Total: <b>${totalCoverage}%</b>:\n\nYour percentage difference: <b>${diff}%</b>`
+      body: `<h3>Code coverage report</h3>Total: <b>${totalCoverage}%</b>:\n\nYour code code coverafe diff: <b>${diff}% ${arrow}</b>`
     });
   }
 };
