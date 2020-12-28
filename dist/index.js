e58a46303eef3e984b0a3e0808d5c74e99559deb
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-12-28 15:54:56 +0000

    remove coloring

--- dist/index.js
+++ dist/index.js
@@ -5910,14 +5910,12 @@ const github = __webpack_require__(438);
 const sendComment = async (token, diff, totalCoverage) => {
   const octokit = github.getOctokit(token);
 
-  const diffColor = diff > 0 ? '#24a148' : '#da1e28';
-
   if (github.context.payload.pull_request) {
     await octokit.issues.createComment({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       issue_number: github.context.payload.pull_request.number,
-      body: `<h3>Code coverage report</h3>Total: <b>${totalCoverage}%</b>:\n\nYour percentage difference: <b style="color:${diffColor};">${diff}%</b>`
+      body: `<h3>Code coverage report</h3>Total: <b>${totalCoverage}%</b>:\n\nYour percentage difference: <b>${diff}%</b>`
     });
   }
 };
