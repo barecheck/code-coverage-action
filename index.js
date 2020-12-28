01c2ae591818d3465973134f8be5e36990ea7032
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-12-28 10:27:23 +0000

    add head lcov file to param (#2)

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const github = require('@actions/github');
 try {
   // `lcov-file` input defined in action metadata file
   const fileName = core.getInput('lcov-file');
+  const headFileName = core.getInput('head-lcov-file');
   console.log(`File name ${fileName}!`);
+  console.log(`Head File name ${headFileName}!`);
+
   const time = new Date().toTimeString();
   core.setOutput('time', time);
   // Get the JSON webhook payload for the event that triggered the workflow
