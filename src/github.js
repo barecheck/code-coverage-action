d7198e091a6d29fdcbaad9498308c1f6f2b58c61
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-12-28 15:50:47 +0000

    add diff color

--- src/github.js
+++ src/github.js
@@ -3,12 +3,14 @@ const github = require('@actions/github');
 const sendComment = async (token, diff, totalCoverage) => {
   const octokit = github.getOctokit(token);
 
+  const diffColor = diff > 0 ? '#24a148' : '#da1e28';
+
   if (github.context.payload.pull_request) {
     await octokit.issues.createComment({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       issue_number: github.context.payload.pull_request.number,
-      body: `<h4>Code coverage report</h4>Total: <b>${totalCoverage}%</b>:\n\nYour percentage difference: <b>${diff}%</b>`
+      body: `<h3>Code coverage report</h3>Total: <b>${totalCoverage}%</b>:\n\nYour percentage difference: <b style="color:${diffColor};">${diff}%</b>`
     });
   }
 };
