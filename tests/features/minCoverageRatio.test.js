f5b1bffe2ab0f4d87acaaa779455f81ef2e04984
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2020-12-31 17:49:51 +0000

    comment tests to check coverage

--- tests/features/minCoverageRatio.test.js
+++ tests/features/minCoverageRatio.test.js
@@ -1,66 +1,66 @@
-const proxyquire = require('proxyquire');
-const sinon = require('sinon');
-const { assert } = require('chai');
+// const proxyquire = require('proxyquire');
+// const sinon = require('sinon');
+// const { assert } = require('chai');
 
-// TODO: create default @actions/core stub that might be reused through all tests
-const defaultMocks = {
-  info: sinon.spy()
-};
+// // TODO: create default @actions/core stub that might be reused through all tests
+// const defaultMocks = {
+//   info: sinon.spy()
+// };
 
-const minCoverageRatioMock = (mocks) => {
-  const { getInput, info } = { ...defaultMocks, ...mocks };
-  return proxyquire('../../src/features/minCoverageRatio', {
-    '@actions/core': { getInput, info }
-  });
-};
+// const minCoverageRatioMock = (mocks) => {
+//   const { getInput, info } = { ...defaultMocks, ...mocks };
+//   return proxyquire('../../src/features/minCoverageRatio', {
+//     '@actions/core': { getInput, info }
+//   });
+// };
 
-describe('eatures/minCoverageRatio', () => {
-  describe('checkCoverageRation', () => {
-    it("error shouldn't be thrown once feature is not enabled", () => {
-      const minCoverageRatio = NaN;
-      const coverageDiff = 0;
+// describe('eatures/minCoverageRatio', () => {
+//   describe('checkCoverageRation', () => {
+//     it("error shouldn't be thrown once feature is not enabled", () => {
+//       const minCoverageRatio = NaN;
+//       const coverageDiff = 0;
 
-      const getInput = sinon
-        .stub()
-        .withArgs('minimum-coverage-ratio')
-        .returns(minCoverageRatio);
+//       const getInput = sinon
+//         .stub()
+//         .withArgs('minimum-coverage-ratio')
+//         .returns(minCoverageRatio);
 
-      const { checkCoverageRation } = minCoverageRatioMock({ getInput });
-      const res = checkCoverageRation(coverageDiff);
+//       const { checkCoverageRation } = minCoverageRatioMock({ getInput });
+//       const res = checkCoverageRation(coverageDiff);
 
-      assert.isUndefined(res);
-    });
+//       assert.isUndefined(res);
+//     });
 
-    it("error shouldn't be thrown once coverage diff is zero", () => {
-      const minCoverageRatio = 0;
-      const coverageDiff = 0;
+//     it("error shouldn't be thrown once coverage diff is zero", () => {
+//       const minCoverageRatio = 0;
+//       const coverageDiff = 0;
 
-      const getInput = sinon
-        .stub()
-        .withArgs('minimum-coverage-ratio')
-        .returns(minCoverageRatio);
+//       const getInput = sinon
+//         .stub()
+//         .withArgs('minimum-coverage-ratio')
+//         .returns(minCoverageRatio);
 
-      const { checkCoverageRation } = minCoverageRatioMock({ getInput });
-      const res = checkCoverageRation(coverageDiff);
+//       const { checkCoverageRation } = minCoverageRatioMock({ getInput });
+//       const res = checkCoverageRation(coverageDiff);
 
-      assert.isUndefined(res);
-    });
+//       assert.isUndefined(res);
+//     });
 
-    it('should thow error once coverage is less than minimum coverage ratio', () => {
-      const minCoverageRatio = 5;
-      const coverageDiff = -10;
+//     it('should thow error once coverage is less than minimum coverage ratio', () => {
+//       const minCoverageRatio = 5;
+//       const coverageDiff = -10;
 
-      const getInput = sinon
-        .stub()
-        .withArgs('minimum-coverage-ratio')
-        .returns(minCoverageRatio);
+//       const getInput = sinon
+//         .stub()
+//         .withArgs('minimum-coverage-ratio')
+//         .returns(minCoverageRatio);
 
-      const { checkCoverageRation } = minCoverageRatioMock({ getInput });
+//       const { checkCoverageRation } = minCoverageRatioMock({ getInput });
 
-      assert.throws(
-        () => checkCoverageRation(coverageDiff),
-        'Code coverage is less than minimum code coverage ratio'
-      );
-    });
-  });
-});
+//       assert.throws(
+//         () => checkCoverageRation(coverageDiff),
+//         'Code coverage is less than minimum code coverage ratio'
+//       );
+//     });
+//   });
+// });
--- tests/lcov.test.js
+++ tests/lcov.test.js
@@ -1,54 +1,54 @@
-const lcov = require('../src/lcov');
-const { assert } = require('chai');
+// const lcov = require('../src/lcov');
+// const { assert } = require('chai');
 
-describe('lcov', () => {
-  describe('percentage', () => {
-    it('should return percentage based on received lines', () => {
-      const parsedLcovFile = [
-        {
-          lines: {
-            hit: 5,
-            found: 10
-          }
-        },
-        {
-          lines: {
-            hit: 5,
-            found: 10
-          }
-        },
-        {
-          lines: {
-            hit: 10,
-            found: 20
-          }
-        }
-      ];
+// describe('lcov', () => {
+//   describe('percentage', () => {
+//     it('should return percentage based on received lines', () => {
+//       const parsedLcovFile = [
+//         {
+//           lines: {
+//             hit: 5,
+//             found: 10
+//           }
+//         },
+//         {
+//           lines: {
+//             hit: 5,
+//             found: 10
+//           }
+//         },
+//         {
+//           lines: {
+//             hit: 10,
+//             found: 20
+//           }
+//         }
+//       ];
 
-      const res = lcov.percentage(parsedLcovFile);
+//       const res = lcov.percentage(parsedLcovFile);
 
-      assert.deepEqual(res, '50.00');
-    });
+//       assert.deepEqual(res, '50.00');
+//     });
 
-    it('result should be fixed to 2 symbols after comma', () => {
-      const parsedLcovFile = [
-        {
-          lines: {
-            hit: 3,
-            found: 10
-          }
-        },
-        {
-          lines: {
-            hit: 23,
-            found: 51
-          }
-        }
-      ];
+//     it('result should be fixed to 2 symbols after comma', () => {
+//       const parsedLcovFile = [
+//         {
+//           lines: {
+//             hit: 3,
+//             found: 10
+//           }
+//         },
+//         {
+//           lines: {
+//             hit: 23,
+//             found: 51
+//           }
+//         }
+//       ];
 
-      const res = lcov.percentage(parsedLcovFile);
+//       const res = lcov.percentage(parsedLcovFile);
 
-      assert.deepEqual(res, '42.62');
-    });
-  });
-});
+//       assert.deepEqual(res, '42.62');
+//     });
+//   });
+// });
