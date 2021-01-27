b3f2767804e67b7739dfe7baa20fc41f2a693a96
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-01-27 23:41:19 +0000

    add lcov functions to get uncovered lines

--- tests/lcov.test.js
+++ tests/lcov.test.js
@@ -1,4 +1,8 @@
-const lcov = require('../src/lcov');
+const {
+  percentage,
+  getUncoveredFilesLines,
+  getGroupedUncoveredFileLines
+} = require('../src/lcov');
 const { assert } = require('chai');
 
 describe('lcov', () => {
@@ -25,7 +29,7 @@ describe('lcov', () => {
         }
       ];
 
-      const res = lcov.percentage(parsedLcovFile);
+      const res = percentage(parsedLcovFile);
 
       assert.deepEqual(res, '50.00');
     });
@@ -46,9 +50,129 @@ describe('lcov', () => {
         }
       ];
 
-      const res = lcov.percentage(parsedLcovFile);
+      const res = percentage(parsedLcovFile);
 
       assert.deepEqual(res, '42.62');
+    });
+  });
+
+  describe('getUncoveredFilesLines', () => {
+    it('should return uncovered lines with file', () => {
+      const lcovData = [
+        {
+          lines: {
+            found: 16,
+            hit: 10,
+            details: [
+              { line: 1, hit: 1 },
+              { line: 3, hit: 1 },
+              { line: 4, hit: 0 },
+              { line: 5, hit: 0 },
+              { line: 6, hit: 0 },
+              { line: 7, hit: 0 },
+              { line: 8, hit: 0 },
+              { line: 10, hit: 0 },
+              { line: 15, hit: 1 },
+              { line: 16, hit: 2 },
+              { line: 17, hit: 2 },
+              { line: 18, hit: 2 },
+              { line: 19, hit: 5 },
+              { line: 20, hit: 5 },
+              { line: 23, hit: 2 },
+              { line: 26, hit: 1 }
+            ]
+          },
+          file: 'src/lcov.js'
+        }
+      ];
+
+      const res = getUncoveredFilesLines(lcovData);
+
+      assert.deepEqual(res, [
+        {
+          file: 'src/lcov.js',
+          lines: [4, 5, 6, 7, 8, 10]
+        }
+      ]);
+    });
+
+    it('should return empty array', () => {
+      const lcovData = [];
+      const res = getUncoveredFilesLines(lcovData);
+
+      assert.isEmpty(res);
+    });
+
+    it('should return empty array once all lines are covered', () => {
+      const lcovData = [
+        {
+          lines: {
+            found: 16,
+            hit: 10,
+            details: [
+              { line: 1, hit: 1 },
+              { line: 3, hit: 2 }
+            ]
+          },
+          file: 'src/lcov.js'
+        }
+      ];
+      const res = getUncoveredFilesLines(lcovData);
+
+      assert.isEmpty(res);
+    });
+  });
+
+  describe('getGroupedUncoveredFileLines', () => {
+    it('should return grouped lines array', () => {
+      const filesLines = [
+        {
+          file: 'src/lcov.js',
+          lines: [4, 5, 6, 7, 8, 10]
+        }
+      ];
+
+      const res = getGroupedUncoveredFileLines(filesLines);
+      assert.deepEqual(res, [
+        {
+          file: 'src/lcov.js',
+          lines: [[4, 8], 10]
+        }
+      ]);
+    });
+
+    it('should return grouped lines array along with single values', () => {
+      const filesLines = [
+        {
+          file: 'src/lcov.js',
+          lines: [4, 5, 6, 7, 8, 10, 14, 15]
+        }
+      ];
+
+      const res = getGroupedUncoveredFileLines(filesLines);
+      assert.deepEqual(res, [
+        {
+          file: 'src/lcov.js',
+          lines: [[4, 8], 10, [14, 15]]
+        }
+      ]);
+    });
+
+    it('should return grouped lines array along with single values at the end', () => {
+      const filesLines = [
+        {
+          file: 'src/lcov.js',
+          lines: [4, 6, 7, 8, 10, 14, 15, 19]
+        }
+      ];
+
+      const res = getGroupedUncoveredFileLines(filesLines);
+      assert.deepEqual(res, [
+        {
+          file: 'src/lcov.js',
+          lines: [4, [6, 8], 10, [14, 15], 19]
+        }
+      ]);
     });
   });
 });
