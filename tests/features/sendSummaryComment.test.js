73e14e0dd76bda033bb79949ccaa39c14e1dc509
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-01-28 09:20:13 +0000

    fix tests

--- tests/features/sendSummaryComment.test.js
+++ tests/features/sendSummaryComment.test.js
@@ -23,7 +23,7 @@ const sendSummaryCommentMock = (mocks) => {
   return proxyquire('../../src/features/sendSummaryComment', {
     '@actions/core': { getInput, info },
     '@actions/github': { getOctokit: () => octokit, context },
-    '../lcov': { uncoveredFileLinesByFileNames: () => null }
+    '../lcov': { uncoveredFileLinesByFileNames: () => [] }
   });
 };
 
