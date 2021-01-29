37ddfa75102f8390cf753932efe88ae719ffd2c7
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-01-29 13:59:07 +0000

    cover lcov function with tests

--- tests/lcov.test.js
+++ tests/lcov.test.js
@@ -1,11 +1,64 @@
+const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const { assert } = require('chai');
+
 const {
   percentage,
   getUncoveredFilesLines,
-  getGroupedUncoveredFileLines
+  getGroupedUncoveredFileLines,
+  uncoveredFileLinesByFileNames
 } = require('../src/lcov');
-const { assert } = require('chai');
+
+const defaultMocks = {
+  lcov: () => null
+};
+
+const lcovMock = (mocks) => {
+  const { lcov } = { ...defaultMocks, ...mocks };
+
+  return proxyquire('../src/lcov', {
+    'lcov-parse': lcov
+  });
+};
 
 describe('lcov', () => {
+  describe('parse', () => {
+    it('should resolve promise with lcov result', async () => {
+      const lcovRes = { test: 2 };
+      const lcovFileData = { someData: 43 };
+
+      const lcov = (data, cb) => {
+        assert.equal(lcovFileData, data);
+        cb(null, lcovRes);
+      };
+
+      const { parse } = lcovMock({ lcov });
+
+      const res = await parse(lcovFileData);
+      assert.deepEqual(res, lcovRes);
+    });
+
+    it('should reject error from lcov lib', async () => {
+      const lcovRes = { test: 2 };
+      const lcovFileData = { someData: 43 };
+      const exprectedErr = new Error('something went wrong');
+
+      const lcov = (data, cb) => {
+        assert.equal(lcovFileData, data);
+        cb(exprectedErr);
+      };
+
+      const { parse } = lcovMock({ lcov });
+
+      try {
+        const res = await parse(lcovFileData);
+        assert.fail('exception should be thrown');
+      } catch (err) {
+        assert.deepEqual(err, exprectedErr);
+      }
+    });
+  });
+
   describe('percentage', () => {
     it('should return percentage based on received lines', () => {
       const parsedLcovFile = [
@@ -171,6 +224,48 @@ describe('lcov', () => {
         {
           file: 'src/lcov.js',
           lines: [4, [6, 8], 10, [14, 15], 19]
+        }
+      ]);
+    });
+  });
+
+  describe('uncoveredFileLinesByFileNames', () => {
+    it('should filter by filename and return grouped values', () => {
+      const lcovData = [
+        {
+          lines: {
+            found: 16,
+            hit: 10,
+            details: [
+              { line: 1, hit: 0 },
+              { line: 2, hit: 0 },
+              { line: 5, hit: 0 },
+              { line: 7, hit: 0 },
+              { line: 8, hit: 3 }
+            ]
+          },
+          file: 'src/test.js'
+        },
+        {
+          lines: {
+            found: 16,
+            hit: 10,
+            details: [
+              { line: 1, hit: 1 },
+              { line: 3, hit: 2 }
+            ]
+          },
+          file: 'src/test2.js'
+        }
+      ];
+      const fileNames = ['src/test.js'];
+
+      const res = uncoveredFileLinesByFileNames(fileNames, lcovData);
+
+      assert.deepEqual(res, [
+        {
+          file: 'src/test.js',
+          lines: [[1, 2], 5, 7]
         }
       ]);
     });
