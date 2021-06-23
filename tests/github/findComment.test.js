79a47c81ab5797f2c1c6c644fcf11344e4cd3a88
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2021-06-23 21:44:33 +0100

    revert

--- tests/github/findComment.test.js
+++ tests/github/findComment.test.js
@@ -53,37 +53,37 @@ describe("github/findComment", () => {
     assert.isUndefined(res);
   });
 
-  // it("should find one comment by text", async () => {
-  //   const bodyText = "some";
-  //   const body = "some text";
-  //   const repo = "tesstRepo";
-  //   const owner = "testOwner";
-  //   const pullNumber = 123;
+  it("should find one comment by text", async () => {
+    const bodyText = "some";
+    const body = "some text";
+    const repo = "tesstRepo";
+    const owner = "testOwner";
+    const pullNumber = 123;
 
-  //   const findCommentRes = { data: [{ body }] };
-  //   const github = {
-  //     context: {
-  //       repo: { repo, owner },
-  //       payload: { pull_request: { number: pullNumber } }
-  //     }
-  //   };
+    const findCommentRes = { data: [{ body }] };
+    const github = {
+      context: {
+        repo: { repo, owner },
+        payload: { pull_request: { number: pullNumber } }
+      }
+    };
 
-  //   const octokitClient = { request: sinon.stub().returns(findCommentRes) };
+    const octokitClient = { request: sinon.stub().returns(findCommentRes) };
 
-  //   const findComment = findCommentMock({ octokitClient, github });
+    const findComment = findCommentMock({ octokitClient, github });
 
-  //   const res = await findComment(bodyText);
+    const res = await findComment(bodyText);
 
-  //   assert.isTrue(octokitClient.request.calledOnce);
-  //   assert.deepEqual(octokitClient.request.firstCall.args, [
-  //     "GET /repos/{owner}/{repo}/issues/{issue_number}/comments",
-  //     {
-  //       repo,
-  //       owner,
-  //       issue_number: 123,
-  //       per_page: 100
-  //     }
-  //   ]);
-  //   assert.deepEqual(res, findCommentRes.data[0]);
-  // });
+    assert.isTrue(octokitClient.request.calledOnce);
+    assert.deepEqual(octokitClient.request.firstCall.args, [
+      "GET /repos/{owner}/{repo}/issues/{issue_number}/comments",
+      {
+        repo,
+        owner,
+        issue_number: 123,
+        per_page: 100
+      }
+    ]);
+    assert.deepEqual(res, findCommentRes.data[0]);
+  });
 });
