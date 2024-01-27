4d814ca364c3c440f66ff7f18ac25047d60d4aa9
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2024-01-27 09:28:15 +0000

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,44 @@
 module.exports = {
-  extends: ["eslint-config-barecheck-base"],
-  rules: {}
+  env: {
+    node: true,
+    mocha: true
+  },
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "plugin:import/errors",
+    "plugin:import/warnings"
+  ],
+  plugins: ["prettier", "simple-import-sort"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-console": 1,
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    // Code complexity rules
+    complexity: ["error", 10],
+    "max-depth": ["error", 3],
+    "max-nested-callbacks": ["error", 3],
+    "max-statements": ["error", 10],
+    "max-lines": ["error", 300]
+  },
+  overrides: [
+    // Code complexity rules for test files
+    {
+      files: ["*.test.js"],
+      rules: {
+        "max-depth": ["error", 2],
+        "max-nested-callbacks": ["error", 4],
+        "max-statements": ["error", 30],
+        "max-lines": ["error", 500]
+      }
+    }
+  ]
 };
